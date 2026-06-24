361012c8f46140975b20f5fd90792699f67fb1c3
Sourangshu Paul <14earcs0158@aryacollege.in>
2026-06-24 16:57:34 +0530

    Add fizzbuzz starter

---
+++ main.py
@@ -0,0 +1,11 @@
+def fizzbuzz(n: int) -> None:
+    """TODO: implement FizzBuzz from 1 to n.
+    Multiples of 3 -> Fizz
+    Multiples of 5 -> Buzz
+    Multiples of both -> FizzBuzz
+    """
+    pass
+
+
+if __name__ == "__main__":
+    fizzbuzz(20)
